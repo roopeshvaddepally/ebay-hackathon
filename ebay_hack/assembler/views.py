77cfdea19ea71a0a33e94807df0c849646aff3c7
Utkarsh Sengar <utkarsh2012@gmail.com>
2011-05-20 02:29:31 -0700

    updated views.py and index.html

--- ebay_hack/assembler/views.py
+++ ebay_hack/assembler/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
 
 def index(request):
-    return HttpResponse("Hello, world.")
+    print request.GET
+    return render_to_response("index.html", {"p": request.GET})
