8855a433ca28778238647438686943e6bfb2ffd6
Gireesh Sreepathi <gisripa@gmail.com>
2011-05-20 05:46:03 -0700

    added views first code

--- ebay_hack/assembler/views.py
+++ ebay_hack/assembler/views.py
@@ -1,14 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
+from calls import findItemsByKeywords as find
+from json import loads
+
 
 def index(request):
     if request.method == "GET":
         # call get method
-        return do_get(request)
+        return HttpResponse("hello")
+        # do_get(request)
     elif request.method == "POST":
         # call post method
         return do_post(request)
-
 
 def do_get(request):
     if request.GET:
@@ -34,7 +37,7 @@ def do_get(request):
         elif request.GET["type"] == "drive":
             return get_drive_list(request)
         else:
-            return get_404_error(request)
+            return render_to_response("index.html", {"p": request.GET})
     else:
         return render_to_response("index.html", {"p": request.GET})
 
@@ -42,7 +45,14 @@ def do_get(request):
     
 def get_ram_list(request):
     # call the api for searching ram
-    return render_to_response("index.html",{"p": "ram list"})
+    response = find("4GB")
+    a = loads(response)
+    result = []
+    try:
+        b = a.["findItemsByKeywordsResponse"][0]["searchResult"][0]
+        result = [(each['itemId'][0], each['title'][0], each['location'][0]) for each in b['item']]
+    except: pass
+    return render_to_response("index.html",{"result": result})
 
 def get_hdd_list(request):
     return render_to_response("index.html",{"p": "hdd list"})
@@ -54,7 +64,23 @@ def get_keyboard_list(request):
     return render_to_response("index.html",{"p": "keyboard_list"})
 
 
+def get_mouse_list(request):
+    pass
 
+def get_motherboard_list(request):
+    pass
+
+
+def get_processor_list(request):
+    pass
+
+
+def get_case_list(request):
+    pass
+
+
+def get_404_error(request):
+    return render_to_response("index.html",{"p": "keyboard_list"})
 
     
 
