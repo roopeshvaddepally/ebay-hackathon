10f11311f6445b08dab7de73a2cd8c3c19a70528
Utkarsh Sengar <utkarsh2012@gmail.com>
2011-05-20 03:29:45 -0700

    updated html

--- ebay_hack/assembler/views.py
+++ ebay_hack/assembler/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render_to_response
 
 def index(request):
     print request.GET
-    return render_to_response("index.html", {"p": request.GET})
+    return render_to_response("assembler/index.html", {"p": request.GET})
--- ebay_hack/settings.py
+++ ebay_hack/settings.py
@@ -45,18 +45,18 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = '/Users/utkarsh/Desktop/ebay-hackathon/ebay_hack/assembler/templates/'
+MEDIA_ROOT = ''
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = 'http://localhost:8000/assembler/templates/'
+MEDIA_URL = ''
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/Users/utkarsh/Desktop/ebay-hackathon/ebay_hack/templates/assembler/static'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -106,6 +106,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    "/Users/utkarsh/Desktop/ebay-hackathon/ebay_hack/templates/"
 )
 
 INSTALLED_APPS = (
