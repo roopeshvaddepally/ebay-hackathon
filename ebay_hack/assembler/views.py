b350ae852465b90c2f20865c6dd2ca3463287a71
Gireesh Sreepathi <gisripa@gmail.com>
2011-05-20 02:30:47 -0700

    added something

--- ebay_hack/assembler/views.py
+++ ebay_hack/assembler/views.py
@@ -1,4 +1,18 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world.")
+    if request.method == "GET":
+        # call get method
+        do_get(request)
+    elif request.method == "POST":
+        # call post method
+        do_post(request)
+
+
+def do_get(request):
+    query_type = request.GET["type"]
+    print query_type
+
+
+    
+
